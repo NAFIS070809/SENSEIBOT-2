7e52886cffff3237f2276bc978f80c4fea238026
Nafis <82797043+NAFIS070809@users.noreply.github.com>
2021-06-10 07:20:23 +0700

    Add files via upload

---
+++ group/donasi.js
@@ -0,0 +1,13 @@
+const donasi = (pushname, prefix) => { 
+	return `
+┏━━━━━━━━━━━━━━━━━━━━
+┃       𝗗𝗢𝗡𝗔𝗦𝗜  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ *Donasi Untuk SENSEIBOT ❉⊰━━✿
+┃  
+┣━⊱ *PULSA*
+┣⊱ _+6283159296996_
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
